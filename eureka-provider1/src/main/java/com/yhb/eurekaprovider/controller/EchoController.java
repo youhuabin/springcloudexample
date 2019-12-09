c0bcd1e1cf98b45c5bedaff2bcb1cf08c0930992
youhb <youhb@tellhow.com>
2019-12-09 16:38:40 +0800

    add sleuth\zipkin

--- eureka-provider1/src/main/java/com/yhb/eurekaprovider/controller/EchoController.java
+++ eureka-provider1/src/main/java/com/yhb/eurekaprovider/controller/EchoController.java
@@ -1,5 +1,7 @@
 package com.yhb.eurekaprovider.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EchoController {
 
+    private Logger log = LoggerFactory.getLogger(EchoController.class);
+
     @Value("${server.port}")
     Integer port;
 
     @RequestMapping("/hi")
     public String hi(@RequestParam String name) {
+        log.info("-->invoke provider1 hi method");
         return "hi "+name+",i am from port:" +port;
     }
 }
